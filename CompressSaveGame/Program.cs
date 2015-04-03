8a3a146713e9ba2b4519f740dede293ab87906a5
Sebastian Hartte <sebastian@hartte.de>
2015-04-03 20:26:38 +0200

    Initial commit.

---
+++ CompressSaveGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TempleFileFormats.SaveGames.Archive;
+
+namespace CompressSaveGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: CompressSaveGame <dir> <savename>");
+                return;
+            }
+
+            var dir = args[0];
+            var name = args[1];
+
+            var tfaiName = name + ".tfai";
+            var tfafName = name + ".tfaf";
+
+            ArchiveWriter.Compress(tfaiName, tfafName, dir);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TempleFileFormats/Utils/BinaryReaderUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempleFileFormats.Utils
+{
+    static class BinaryReaderUtils
+    {
+        /// <summary>
+        /// Reads a string from the stream that is prefixed with a 32-bit integer containing its length. The
+        /// string is assumed to not be null terminated. The string is decoded using the default encoding for
+        /// this platform.
+        /// </summary>
+        /// <param name="reader">The reader to read from.</param>
+        /// <returns>The string read from the reader.</returns>
+        public static string ReadPrefixedString(this BinaryReader reader)
+        {
+            var length = reader.ReadInt32();
+            var data = reader.ReadBytes(length);
+
+            // Decode using local encoding
+            return Encoding.Default.GetString(data);
+        }
+
+    }
+}
